19c5c5e9788a4ac0593fe3028ab038fc6bf99ed6
Thomas Lombart <t.lombart97@gmail.com>
2017-07-03 22:29:09 +0200

    Delete log console

--- server/sockets.js
+++ server/sockets.js
@@ -5,7 +5,6 @@ const QUANDL_API_KEY = process.env.QUANDL_API_KEY;
 
 module.exports = io => {
   io.on('connection', socket => {
-    console.log(socket.id)
     socket.on('send stock code', code => {
       axios.get(`https://www.quandl.com/api/v3/datasets/WIKI/${code}/data.json?api_key=${QUANDL_API_KEY}`).then(data => {
         return io.emit('receive stock code', code);
