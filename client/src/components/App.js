9c8d29e181c6da8ac7f71c4a86a2d3984fffcf82
Thomas Lombart <t.lombart97@gmail.com>
2017-07-05 20:52:49 +0200

    Add highcharts into app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,14 +2,23 @@ import React, { Component } from 'react';
 import Chart from './Chart';
 import StockList from './StockList';
 import AddStock from './AddStock';
+import Footer from './Footer';
+import '../styles/app.css';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {pieData: [{name: "Firefox",y: 6},{name: "MSIE",y: 4},{name: "Safari",y: 4},{name: "Opera",y: 1},{name: "Chrome",y: 7}]}
+  }
   render() {
     return (
-      <div>
-        <Chart/>
+      <div className="main">
+        <h1 className="title">Watch the stocks</h1>
+        <Chart data={this.state.pieData}/>
         <StockList/>
         <AddStock/>
+        <Footer/>
       </div>
     );
   }
