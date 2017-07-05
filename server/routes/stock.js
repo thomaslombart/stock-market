3c6458ba0e93885b85c4a21bc92bc54ef1130411
Thomas Lombart <t.lombart97@gmail.com>
2017-07-05 20:52:33 +0200

    Start to clarify call to API

--- server/routes/stock.js
+++ server/routes/stock.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import axios from 'axios';
 import Stock from '../models/stock';
+import {getFinancialData} from '../helpers';
 
 const router = express.Router();
 
@@ -17,8 +18,13 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   Stock.findOne({code: req.body.code}, (err, doc) => {
     if (!doc) {
-      let stock = new Stock({code: req.body.code});
-      stock.save(err => err ? res.sendStatus(500) : res.sendStatus(200));
+      // We need to verify if the stock code is a valid one by calling Quandl API
+      axios.get(getFinancialData(req.body.code)).then(response => {
+        if (response.status === 200) {
+          let stock = new Stock({code: req.body.code});
+          stock.save(err => err ? res.sendStatus(500) : res.sendStatus(200));
+        }
+      });
     }
   });
 });
--- server/sockets.js
+++ server/sockets.js
@@ -1,4 +1,3 @@
-import {getFinancialData} from './helpers';
 import axios from 'axios';
 
 const QUANDL_API_KEY = process.env.QUANDL_API_KEY;
@@ -6,9 +5,7 @@ const QUANDL_API_KEY = process.env.QUANDL_API_KEY;
 module.exports = io => {
   io.on('connection', socket => {
     socket.on('send stock code', code => {
-      axios.get(`https://www.quandl.com/api/v3/datasets/WIKI/${code}/data.json?api_key=${QUANDL_API_KEY}`).then(data => {
-        return io.emit('receive stock code', code);
-      }).catch(error => console.log(error));
+      return io.emit('receive stock code', code);
     });
     socket.on('send delete stock code', id => {
       return io.emit('delete stock code', id);
