56170b7bc16921f54f9439456984e345fff94756
Thomas Lombart <t.lombart97@gmail.com>
2017-07-14 18:12:13 +0200

    Add export components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import Chart from './Chart';
-import Loading from './Loading';
-import StockList from './StockList';
-import AddStock from './AddStock';
-import Footer from './Footer';
+import { Chart, Loading, StockList, AddStock, Footer } from './';
 import '../styles/app.css';
 
 const App = () => (
-  <div className="main container">
+  <div className="main container-fluid">
     <div className="row justify-content-center">
       <div className="col-sm-12">
         <h1 className="title text-sm-center">Watch the stocks</h1>
@@ -25,6 +21,7 @@ const App = () => (
         <AddStock />
       </div>
       <div className="col-sm-12">
+        <hr />
         <Footer />
       </div>
     </div>
---
+++ client/src/components/index.js
@@ -0,0 +1,12 @@
+import App from './App';
+import AddStock from './AddStock';
+import Chart from './Chart';
+import Footer from './Footer';
+import Loading from './Loading';
+import Root from './Root';
+import Stock from './Stock';
+import StockList from './StockList';
+
+export {
+    App, AddStock, Chart, Footer, Loading, Root, Stock, StockList
+};
