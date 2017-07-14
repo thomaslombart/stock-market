d57ea0f8511358248e0cc62fbb7bd6651fead946
Thomas Lombart <t.lombart97@gmail.com>
2017-07-14 16:08:17 +0200

    Simplify arrow functions

--- client/src/actions/stock.js
+++ client/src/actions/stock.js
@@ -15,51 +15,51 @@ export const socket = require('socket.io-client').connect();
 /* socket listeners */
 
 socket.on('fetch data', () => {
-  store.dispatch({type: FETCH_DATA});
+  store.dispatch({ type: FETCH_DATA });
 });
 
-socket.on('load stocks', res => {
-  store.dispatch({type: LOAD_STOCKS, stocks: res});
-  store.dispatch({type: SUCCESS_STOCK});
+socket.on('load stocks', (res) => {
+  store.dispatch({ type: LOAD_STOCKS, stocks: res });
+  store.dispatch({ type: SUCCESS_STOCK });
 });
 
-socket.on('add stock code', res => {
-  store.dispatch({type: ADD_STOCK, code: res.code, description: res.description, data: res.data, id: res.id});
-  store.dispatch({type: SUCCESS_STOCK});
+socket.on('add stock code', (res) => {
+  store.dispatch({ type: ADD_STOCK, code: res.code, description: res.description, data: res.data, id: res.id });
+  store.dispatch({ type: SUCCESS_STOCK });
 });
 
-socket.on('error stock code', message => {
-  store.dispatch({type: ERROR_STOCK, message: message});
+socket.on('error stock code', (message) => {
+  store.dispatch({ type: ERROR_STOCK, message: message });
 });
 
-socket.on('delete stock code', id => {
-  store.dispatch({type: REMOVE_STOCK, id: id});
-  store.dispatch({type: SUCCESS_STOCK});
+socket.on('delete stock code', (id) => {
+  store.dispatch({ type: REMOVE_STOCK, id });
+  store.dispatch({ type: SUCCESS_STOCK });
 });
 
-socket.on('load detailed stock', data => {
-  store.dispatch({type: LOAD_DETAIL_STOCK, ohlc: data.ohlc, volume: data.volume});
+socket.on('load detailed stock', (data) => {
+  store.dispatch({ type: LOAD_DETAIL_STOCK, ohlc: data.ohlc, volume: data.volume });
 });
 
 /* actions */
 
-export const addStock = code => {
-  return dispatch => {
-    dispatch({type: FETCH_DATA});
+export const addStock = (code) => (
+  dispatch => {
+    dispatch({ type: FETCH_DATA });
     return socket.emit('send stock code', code);
   }
-}
+);
 
-export const removeStock = id => {
-  return dispatch => {
-    dispatch({type: FETCH_DATA});
+export const removeStock = (id) => (
+  dispatch => {
+    dispatch({ type: FETCH_DATA });
     return socket.emit('send delete stock code', id);
   }
-}
+);
 
-export const loadDetailedStock = code => {
-  return dispatch => {
-    dispatch({type: FETCH_DATA});
+export const loadDetailedStock = (code) => (
+  dispatch => {
+    dispatch({ type: FETCH_DATA });
     return socket.emit('send detail stock code', code);
   }
-}
+);
