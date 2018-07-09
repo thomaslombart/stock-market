f21a6ecb0f14f9227f43a33e32255971082dfcfa
Thomas Lombart <t.lombart97@gmail.com>
2018-07-09 19:06:23 +0200

    Add styled components and formatting

--- client/src/components/Stock.js
+++ client/src/components/Stock.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import '../styles/stock.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "../styles/stock.css";
+import styled from "styled-components";
+
+const StockTitle = styled.h2`
+  color: blue;
+`;
 
 const Stock = ({ code, description, onClick }) => (
   <div className="stock col-sm-4">
     <p className="stock-code">
+      <StockTitle>{code}</StockTitle>
       <span>{code}</span>
       <span className="float-sm-right" onClick={onClick}>
-        <i className="fa fa-times"></i>
+        <i className="fa fa-times" />
       </span>
     </p>
-    <p className="stock-description">
-      {description}
-    </p>
+    <p className="stock-description">{description}</p>
   </div>
 );
 
--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import path from 'path';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import path from "path";
 
 mongoose.connect(process.env.STOCK_MLAB_URI);
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, "client/build")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Redirect all unmatched routes to homepage
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => console.log(`Express Server is listening on port ${port}`));
+const server = app.listen(port, () =>
+  console.log(`Express Server is listening on port ${port}`)
+);
 
-const io = require('socket.io')(server);
-require('./server/sockets.js')(io);
+const io = require("socket.io")(server);
+require("./server/sockets.js")(io);
